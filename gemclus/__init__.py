0872108555cf47a6a7719966392bbea6f13cd388
lohl <louis.ohl@inria.fr>
2023-05-12 20:01:44 +0200

    Switching to first stable release 0.1.0

--- gemclus/__init__.py
+++ gemclus/__init__.py
@@ -1,4 +1,4 @@
 from gemclus import sparse, linear, mlp, data, nonparametric
 
 __all__ = ['linear', 'mlp', 'sparse', 'data', 'nonparametric', '__version__']
-__version__ = '0.0.2'
+__version__ = '0.1.0'
