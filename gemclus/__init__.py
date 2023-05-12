f203c943809301099e21965038567e169895f67a
lohl <louis.ohl@inria.fr>
2023-05-12 20:00:24 +0200

    FIX: nonparametric module name in init

--- gemclus/__init__.py
+++ gemclus/__init__.py
@@ -1,4 +1,4 @@
 from gemclus import sparse, linear, mlp, data, nonparametric
 
-__all__ = ['linear', 'mlp', 'sparse', 'data', 'non_parametric', '__version__']
+__all__ = ['linear', 'mlp', 'sparse', 'data', 'nonparametric', '__version__']
 __version__ = '0.0.2'
