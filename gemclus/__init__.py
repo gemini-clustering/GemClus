034d9fe1e231414adc6bdcbfbd6ea755b57ad003
lohl <louis.ohl@inria.fr>
2023-05-16 10:25:49 +0200

    FIX: missing link to gemini package

--- gemclus/__init__.py
+++ gemclus/__init__.py
@@ -1,4 +1,4 @@
-from gemclus import sparse, linear, mlp, data, nonparametric
+from gemclus import sparse, linear, mlp, data, nonparametric, gemini
 
-__all__ = ['linear', 'mlp', 'sparse', 'data', 'nonparametric', '__version__']
+__all__ = ['linear', 'mlp', 'sparse', 'data', 'nonparametric', 'gemini','__version__']
 __version__ = '0.1.0'
