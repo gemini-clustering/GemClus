ebf0e14da8a1554239ec914d929d23233d6347b5
lohl <louis.ohl@inria.fr>
2023-09-04 13:49:59 +0200

    FIX: tree in package init

--- gemclus/__init__.py
+++ gemclus/__init__.py
@@ -1,4 +1,4 @@
-from gemclus import sparse, linear, mlp, data, nonparametric, gemini
+from gemclus import sparse, linear, mlp, data, nonparametric, gemini, tree
 
-__all__ = ['linear', 'mlp', 'sparse', 'data', 'nonparametric', 'gemini','__version__']
+__all__ = ['linear', 'mlp', 'sparse', 'data', 'nonparametric', 'gemini', 'tree', '__version__']
 __version__ = '0.1.1'
